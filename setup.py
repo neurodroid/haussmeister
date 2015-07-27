f5b90a99d98a6c911be9eceed6408d8056ee67e3
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2015-07-27 22:25:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 setup(name='haussmeister',
       version='0.1.0',
       packages=['haussmeister'],
       package_dir={'haussmeister': 'haussmeister'},
       package_data={'haussmeister': ['data/*.ttf']},
+      install_requires=[
+          'numpy',
+          'pillow',
+          'tables',
+          'sima'
+      ]
       )
