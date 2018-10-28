aa92c5230bfa30c99779f641117361154ce8b109
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2018-10-28 07:47:05 +0100

    Add package description

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup
-setup(name='haussmeister',
-      version='0.2.0',
-      packages=['haussmeister'],
-      package_dir={'haussmeister': 'haussmeister'},
-      package_data={'haussmeister': ['data/*.ttf']},
-      scripts=['haussmeister/thor2tiff.py'],
-      install_requires=[
-          'numpy',
-          'pillow',
-          'tables',
-          'sima',
-          'bottleneck',
-          'contours'
-      ]
-      )
+setup(
+    name='haussmeister',
+    version='0.2.0',
+    description='Tools for managing imaging data',
+    packages=['haussmeister'],
+    package_dir={'haussmeister': 'haussmeister'},
+    package_data={'haussmeister': ['data/*.ttf']},
+    scripts=['haussmeister/thor2tiff.py'],
+    install_requires=[
+        'numpy',
+        'pillow',
+        'tables',
+        'sima',
+        'bottleneck',
+        'contours'
+    ]
+)
